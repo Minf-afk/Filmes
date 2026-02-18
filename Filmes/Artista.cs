5809e772f4155aeff8e60172a9465e865acc3240
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-18 13:05:30 -0300

    Criação de ctor

--- Filmes/Artista.cs
+++ Filmes/Artista.cs
@@ -6,6 +6,14 @@ namespace Filmes
 {
     public class Artista
     {
-
+        public Artista(string nome, int idade, List<string> filmes)
+        {
+            Nome = nome;
+            Idade = idade;
+            FilmesAtuados = filmes; 
+        }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public List<string> FilmesAtuados { get; set; }
     }
 }
