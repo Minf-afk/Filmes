be7393deb2734c97849aeb66e178b293947d36e8
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-18 12:57:48 -0300

    Adicionar arquivos de projeto.

---
+++ Filmes/Filme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Filmes
+{
+    public class Filme
+    {
+        public Filme(string titulo, int duracao, List<string> elenco)
+        {
+            Titulo = titulo;
+            Duracao = duracao;
+            Elenco = elenco;
+        }
+        public string Titulo { get; set; }
+        public int Duracao { get; set; }
+        public List<string> Elenco { get; set; }
+
+        public void MostrarDetalhes()
+        {
+            Console.WriteLine($"O filme: {Titulo} tem duração de: {Duracao} e conta com o grande elenco: {string.Join(", ",Elenco)}");
+            
+        }
+    }
+}
---
+++ Filmes/Program.cs
@@ -0,0 +1,15 @@
+﻿using Filmes;
+
+Filme harry = new("Harry Potter", 251, new List<string> 
+{ 
+    "Rupert Grint",
+    "Hermione Granger", 
+    "Tom Felton" 
+});
+
+Filme djangoLivre = new("Django Livre", 320, new List<string>
+{
+    ""
+})
+
+harry.MostrarDetalhes();
