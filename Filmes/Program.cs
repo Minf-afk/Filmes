738e8ac56906465942533e5290d748b99f01dba0
Mateus de Santana Pereira <msantana.inf@gmail.com>
2026-02-18 13:09:06 -0300

    Criação de novas instancias

--- Filmes/Program.cs
+++ Filmes/Program.cs
@@ -9,7 +9,15 @@ Filme harry = new("Harry Potter", 251, new List<string>
 
 Filme djangoLivre = new("Django Livre", 320, new List<string>
 {
-    ""
-})
+    "Jamie Foxx",
+    "Kerry Washington",
+    "James Remar"
+});
 
 harry.MostrarDetalhes();
+djangoLivre.MostrarDetalhes();
+
+Artista will = new("Will Smith", 25, new List<string>
+{
+    "Em busca da Felicidade"
+});
